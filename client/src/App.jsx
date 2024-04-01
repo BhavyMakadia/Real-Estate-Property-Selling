66ae4d2652815de298754dd157e13ab66a791621
bhavytimepass <83721689+bhavytimepass@users.noreply.github.com>
2024-04-01 18:29:33 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Routes, Route , useLocation} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
-import ContactUs from './pages/ContactUs'
+import ContactUs from './pages/ContactUs';
 import Search from './pages/Search';
 
 import Profile from './pages/Profile';
@@ -28,14 +28,14 @@ import AdminReview from './components/AdminReview';
 
 import AdminProfile from './components/AdminProfile';
 
-
 export default function App() {
   return (
     <BrowserRouter>
-    <AppContent/>
-       </BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
   );
 }
+
 function AppContent() {
   const location = useLocation();
   const [isAdminRoute, setIsAdminRoute] = useState(location.pathname.startsWith('/admin'));
@@ -43,43 +43,39 @@ function AppContent() {
   useEffect(() => {
     setIsAdminRoute(location.pathname.startsWith('/admin'));
   }, [location.pathname]);
-  
+
   return (
     <>
-      {!isAdminRoute && <Header /> }
+      {!isAdminRoute && <Header />}
       {isAdminRoute && <AdminLayout />}
-      
+
       <Routes>
-      <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contactus' element={<ContactUs />} />
-        <Route path='/signin' element={<SignIn />}/>
-        <Route path='/signup' element={<SignUp  />}/>
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
         <Route path='/search' element={<Search />} />
-        <Route path='/listing/:listingId' element={<Listing/>} />
+        <Route path='/listing/:listingId' element={<Listing />} />
         <Route element={<PrivateRoute />}>
-                <Route path='/profile' element={<Profile />} />
-                <Route path='/createlisting' element={<CreateListing />} />
-                <Route path='/showlisting' element={<Showlisting />} />
-                <Route path='/editlisting/:listingId' element={<EditList />} />
-                
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/createlisting' element={<CreateListing />} />
+          <Route path='/showlisting' element={<Showlisting />} />
+          <Route path='/editlisting/:listingId' element={<EditList />} />
         </Route>
-           <Route path='/admin' element={<AdminHome />} />
-          
-           <Route element={<PrivateRoute />}>
-           <Route path='/adminuser' element={<AdminUser />} />
-           <Route path='/adminreview' element={<AdminReview />} />
-           <Route path='/admincreatelisting' element={<AdminCreateListing />} />
-           <Route path='/adminshowlisting' element={<AdminShowlisting />} />
-                 
-      <Route path='/adminedit' element={<AdminEditlist />} />
-      <Route path='/adminprofile' element={<AdminProfile />} />
-      <Route path='/adminupdatelist/:listingId' element={<Adminupdatelist />} />
-      <Route path='/adminlisting/:listingId' element={<Listing/>} />
-        
-      </Route> 
+        <Route path='/admin' element={<AdminHome />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/adminuser' element={<AdminUser />} />
+          <Route path='/adminreview' element={<AdminReview />} />
+          <Route path='/admincreatelisting' element={<AdminCreateListing />} />
+          <Route path='/adminshowlisting' element={<AdminShowlisting />} />
+          <Route path='/adminedit' element={<AdminEditlist />} />
+          <Route path='/adminprofile' element={<AdminProfile />} />
+          <Route path='/adminupdatelist/:listingId' element={<Adminupdatelist />} />
+          <Route path='/adminlisting/:listingId' element={<Listing />} />
+        </Route>
       </Routes>
-    <Footer />
+      <Footer />
     </>
   );
 }
--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,28 +1,24 @@
-import React from 'react'
-import {FaSearch} from 'react-icons/fa';
-import {useSelector} from 'react-redux';
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+
 export default function Footer() {
   const { currentUser } = useSelector((state) => state.user);
- 
+
   return (
     <footer className="bg-[#83b8b8]  text-center bottom-0 fixed  w-full pl-24  ">
-    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className=" pl-16 text-sm  sm:text-center font-bold ">© 2023 . All Rights Reserved.</span>
-      <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 pr-24">
-      <Link to='/About'> 
-      <li className="hover:underline text-slate-100 hover:text-slate-500 me-4 md:me-6">About
-        </li>
-        </Link>
-  
-       
-
-  <Link to='/Contactus'>
-  <li className='hidden sm:inline text-slate-100 hover:text-slate-500 font-bold me-4 md:me-6 rounded '>Contact Us</li>
-  </Link>
-  
-      </ul>
-    </div>
-  </footer>    
-  )
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className=" pl-16 text-sm  sm:text-center font-bold ">© 2023 . All Rights Reserved.</span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 pr-24">
+          <Link to='/About'>
+            <li className="hover:underline text-slate-100 hover:text-slate-500 me-4 md:me-6">About</li>
+          </Link>
+          <Link to='/Contactus'>
+            <li className='hidden sm:inline text-slate-100 hover:text-slate-500 font-bold me-4 md:me-6 rounded '>Contact Us</li>
+          </Link>
+        </ul>
+      </div>
+    </footer>
+  );
 }
